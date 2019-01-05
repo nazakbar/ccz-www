e9974c5c1265e8a900a4420deb7f44f5e1b70b6d
nazakbar <nazrinakbar15@gmail.com>
2019-01-05 15:36:49 +0800

    Changes made to structure of contact-form

--- src/routes/general/index.js
+++ src/routes/general/index.js
@@ -3,17 +3,6 @@ import { Link } from 'preact-router/match';
 import linkState from 'linkstate';
 import { database } from '../../components/firebase';
 
-import TextArea from '../../components/contactForm/textArea';
-import Input from '../../components/contactForm/inputGroup';
-import Checkbox from '../../components/contactForm/checkbox';
-
-/**TODO - What is missing
- * separation to stateless
- * notice
- * Title
- * back button
- */
-
 class General extends Component {
 	state = {
 		selected: 'inquiries',
@@ -24,7 +13,7 @@ class General extends Component {
 		company: '',
 		title: '',
 		country: '',
-		interest: ''
+		interest: 'false'
 	};
 
 	onChange = (e) => {
@@ -57,7 +46,7 @@ class General extends Component {
 			company: '',
 			title: '',
 			country: '',
-			interest: ''
+			interest: 'false'
 		});
 	}
 	
@@ -67,6 +56,7 @@ class General extends Component {
 		return (
 			<div>
 				<form onSubmit={this.submit}>
+					<Link href="/contact" aria-label="Contact">Contact</Link>
 					<h2>General</h2>
 					<fieldset>
 						<p>Are you contacting us about</p>
@@ -92,18 +82,43 @@ class General extends Component {
 						</div>
 					</fieldset>
 					<fieldset>
-						<TextArea value={message} onInput={linkState(this, 'message')}>Please tell us how we can help you</TextArea>
-						<Input type="text" name="firstName" value={firstName} onInput={linkState(this, 'firstName')}>First Name</Input>
-						<Input type="text" name="lastName" value={lastName} onInput={linkState(this, 'lastName')}>Last Name</Input>
-						<Input type="email" name="email" value={email} onInput={linkState(this, 'email')}>Email</Input>
-						<Input type="text" name="company" value={company} onInput={linkState(this, 'company')}>Company</Input>
-						<Input type="text" name="title" value={title} onInput={linkState(this, 'title')}>Title</Input>
-						<Input type="text" name="country" value={country} onInput={linkState(this, 'country')}>Country</Input>
+						<div>
+							<label htmlFor="message">Please tell us how we can help you</label>
+							<input type="text" name="message" id="message" value={message} onInput={linkState(this, 'message')} />
+						</div>
+						<div>
+							<label htmlFor="firstName">First Name</label>
+							<input type="text" name="firstName" id="firstName" value={firstName} onInput={linkState(this, 'firstName')} />
+						</div>
+						<div>
+							<label htmlFor="lastName">Last Name</label>
+							<input type="text" name="lastName" id="lastName" value={lastName} onInput={linkState(this, 'lastName')} />
+						</div>
+						<div>
+							<label htmlFor="email">Email</label>
+							<input type="email" name="email" id="email" value={email} onInput={linkState(this, 'email')} />
+						</div>
+						<div>
+							<label htmlFor="company">Company</label>
+							<input type="text" name="company" id="company" value={company} onInput={linkState(this, 'company')} />
+						</div>
+						<div>
+							<label htmlFor="title">Title</label>
+							<input type="text" name="title" id="title" value={title} onInput={linkState(this, 'title')} />
+						</div>
+						<div>
+							<label htmlFor="country">Country</label>
+							<input type="text" name="country" id="country" value={country} onInput={linkState(this, 'country')} />
+						</div>
 					</fieldset>
-					<Checkbox name="interest" value={interest} onInput={linkState(this, 'interest')} />
+					<fieldset>
+						<div>
+							<input type="checkbox" name="checkbox_interest" id="checkbox_interest" onInput={linkState(this, 'interest')} />
+							<label htmlFor="checkbox_interest">I am interested in receiving articles and insights from Concrezan.</label>
+						</div>
+					</fieldset>
 					<div>
-						<p>
-							By submitting this form, I consent to the processing of the personal data that I provide Concrezan in accordance with and as described in the <Link href="/">Privacy Policy.</Link>
+						<p> By submitting this form, I consent to the processing of the personal data that I provide Concrezan in accordance with and as described in the <Link href="/privacy-policy" aria-label="Privacy Policy">Privacy Policy</Link>
 						</p>
 					</div>
 					<button type="submit">Submit</button>
